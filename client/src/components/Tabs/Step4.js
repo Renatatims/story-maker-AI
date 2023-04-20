a6716f40c02ee31f27aa7f320de22b6a7d4cf4fd
ReTims <renatatims@gmail.com>
2023-04-20 11:58:59 -0500

    useState to Render the generated image - now an AI image is rendered to the page

--- client/src/components/Tabs/Step4.js
+++ client/src/components/Tabs/Step4.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Button,
   Container,
   TextField,
   Typography,
   Box,
+  CircularProgress,
 } from "@mui/material";
 
 import { Configuration, OpenAIApi } from "openai";
@@ -31,28 +32,47 @@ async function generateImage(prompt) {
 }
 
 const ImageGeneratorPage = () => {
-    generateImage();
+  const [prompt, setPrompt] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+
+    const imagePrompt = prompt;
+    const generatedImageUrl = await generateImage(imagePrompt);
+    setImageUrl(generatedImageUrl);
+
+    setLoading(false);
+  };
+
   return (
     <Container>
       <Typography variant="h4">Step 4: Create a personalized image using AI</Typography>
-      <Box component="form" mt={2}>
+      <Box component="form" onSubmit={handleSubmit} mt={2}>
         <TextField
           label="Enter a description"
           fullWidth
           value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
         />
         <Button
           type="submit"
           color="primary"
           variant="contained"
+          disabled={!prompt || loading}
           style={{ marginTop: '1rem' }}
         >
           Generate Image
         </Button>
       </Box>
+      {loading && <CircularProgress />}
+      {imageUrl && (
         <Box mt={4}>
-          <img alt="ImageGenerated" style={{ maxWidth: '100%' }} />
+          <img src={imageUrl} alt="Generated" style={{ maxWidth: '100%' }} />
         </Box>
+      )}
     </Container>
   );
 };
