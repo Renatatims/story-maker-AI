d972588a5a6c0d97f968c777c351a57665cff0d3
ReTims <renatatims@gmail.com>
2023-04-21 00:19:17 -0500

    Created models directory + User model to save user's info in the database

---
+++ server/models/User.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  storiesAI: [{
+    type: Schema.Types.ObjectId,
+    ref: "Stories",
+  }],
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
