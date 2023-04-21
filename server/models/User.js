5f2f1926b3c103c8881b13ea51fe51ebeca99b80
ReTims <renatatims@gmail.com>
2023-04-21 00:52:14 -0500

    Installed bcrypt package + hashed user's password in User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
   firstName: {
@@ -19,11 +20,29 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
-  storiesAI: [{
-    type: Schema.Types.ObjectId,
-    ref: "Story",
-  }],
+  storiesAI: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Story",
+    },
+  ],
 });
+
+// middleware to create password
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// hashed password compared with user's input
+userSchema.methods.isCorrectPassword = async function (password) {
+  console.log(password);
+  return bcrypt.compare(password, this.password);
+};
 
 const User = model("User", userSchema);
 
