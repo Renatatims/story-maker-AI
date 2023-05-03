989e0ef625f18645fb77794b9cb919cbb2502090
ReTims <renatatims@gmail.com>
2023-05-03 18:23:44 -0500

    Included more AI images generated - princess theme - step2 mobile friendly adjustments

--- client/src/utils/images2.js
+++ client/src/utils/images2.js
@@ -3,21 +3,50 @@ const images = [
       id: 1,
       url: require("../assets/images/img1.PNG"),
       title: "Chirstmas1",
-      author: "pch.vector on Freepik",
+      author: "Dalle",
     },
     {
       id: 2,
       url: require("../assets/images/img2.PNG"),
       title: "Chirstmas2",
-      author: "pch.vector on Freepik",
+      author: "Dalle",
     },
     {
       id: 3,
       url: require("../assets/images/img3.PNG"),
       title: "Chirstmas3",
-      author: "pch.vector on Freepik",
+      author: "Dalle",
     },
-
+    {
+      id: 4,
+      url: require("../assets/images/img4.PNG"),
+      title: "Chirstmas3",
+      author: "Dalle",
+    },
+    {
+      id: 5,
+      url: require("../assets/images/img5.PNG"),
+      title: "Princess1",
+      author: "Dalle",
+    },
+    {
+      id: 6,
+      url: require("../assets/images/img6.PNG"),
+      title: "Princess2",
+      author: "Dalle",
+    },
+    {
+      id: 7,
+      url: require("../assets/images/img7.PNG"),
+      title: "Princess3",
+      author: "Dalle",
+    },
+    {
+      id: 8,
+      url: require("../assets/images/img8.PNG"),
+      title: "Princess4",
+      author: "Dalle",
+    },
   ];
   
   export default images;
